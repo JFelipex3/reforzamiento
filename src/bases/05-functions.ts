02fb0331956e77e824094e57a91d3559af12aaf3
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-09-10 17:18:24 -0300

    Funciones de flecha

--- src/bases/05-functions.ts
+++ src/bases/05-functions.ts
@@ -6,12 +6,16 @@ function greet(name: string): string {
 // Función de flecha
 const greetArrow = (name: string): string => {
     return `Hola ${name}... función de flecha`;
-}
+};
+
+// Función de flecha simplificada
+const greetArrow2 = (name: string): string => `Hola ${name}... función de flecha simplificada`;
 
 const message = greet("Goku");
 const messageArrow = greetArrow("Vegeta");
+const messageArrow2 = greetArrow2("Trunks");
 
-console.log({message, messageArrow});
+console.log({message, messageArrow, messageArrow2});
 
 function getUser() {
     return {
@@ -34,6 +38,7 @@ interface User {
     username: string;
 }
 
+// Función de flecha que retorna un objeto implícito
 const getUserArrow = (): User => ({
     uid: 'ABC123',
     username: 'El_Papi1502'
