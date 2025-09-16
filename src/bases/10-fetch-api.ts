8845fd01bba2f88a61d1b1209ad4769693e09a3e
JFelipex3 <jfelipe.machuca@gmail.com>
2025-09-15 22:16:41 -0300

    Interface y optimización

--- src/bases/10-fetch-api.ts
+++ src/bases/10-fetch-api.ts
@@ -1,18 +1,29 @@
+import type { GiphyRandomResponse } from "../data/giphy.response"; // Se define como type cuando solo se usara como tipo de dato
 
 const API_KEY = 'JqPQQYfxYvrTvDsWNaQPbWgyUtruUCUz';
 
 const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
 
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+    document.body.append(imgElement);
+}
+
 myRequest
     .then( (resp) => resp.json() )
-    .then( (data) => {
-        const imageUrl = data.data.images.original.url;
-        console.log(imageUrl);
+    .then( ( {data} : GiphyRandomResponse) => {
+        // const imageUrl = data.data.images.original.url;
+        // console.log(imageUrl);
 
-        const imgElement = document.createElement('img');
-        imgElement.src = imageUrl;
+        // const imgElement = document.createElement('img');
+        // imgElement.src = imageUrl;
 
-        document.body.append(imgElement);
+        // document.body.append(imgElement);
+
+        const imageUrl = data.images.original.url;
+        createImageInsideDOM(imageUrl);
+
     })
     .catch( (err) => {
         console.error(err);
